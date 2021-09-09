65d81bd7ed706865314e1b96db6749b301a42edf
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-09-09 18:10:36 +0100

    hmm

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const app = express()
 
-app.get('/u/:user', (req, res) => {
+app.get('/u:user', (req, res) => {
   res.redirect(301, `https://scratch.mit.edu/users/${req.params.user}`)
 })
 
-app.get('/p/:id', (req, res) => {
+app.get('/p:id', (req, res) => {
   res.redirect(301, `https://scratch.mit.edu/projects/${req.params.id}`)
 })
 
-app.get('/s/:id', (req, res) => {
+app.get('/s:id', (req, res) => {
   res.redirect(301, `https://scratch.mit.edu/studios/${req.params.id}`)
 })
 
-app.get('/o/:user', (req, res) => {
+app.get('/o:user', (req, res) => {
   res.redirect(301, `https://ocular.jeffalo.net/user/${req.params.user}`)
 })
 
-app.get('/tw/:id', (req, res) => {
+app.get('/tw:id', (req, res) => {
   res.redirect(301, `https://turbowarp.org/${req.params.id}`)
 })
 
